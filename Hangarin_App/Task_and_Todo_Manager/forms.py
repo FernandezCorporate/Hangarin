c585b07c26ec5f2ba1db26b09437ce812b35fb65
AllenFernandez <202380211@psu.palawan.edu.ph>
2026-02-28 23:20:56 +0800

    Form layout and functionality for Task added

---
+++ Hangarin_App/Task_and_Todo_Manager/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Task
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            'deadline': forms.DateTimeInput(
+                attrs={
+                    'type': 'datetime-local',
+                    'class': 'form-control'
+                }
+            ),
+        }
--- Hangarin_App/Task_and_Todo_Manager/views.py
+++ Hangarin_App/Task_and_Todo_Manager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from Task_and_Todo_Manager.models import Task
+from Task_and_Todo_Manager.forms import TaskForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Task
@@ -12,3 +15,9 @@ class TaskView(ListView):
     context_object_name = 'task'
     template_name = 'taskList.html'
     paginate_by = 5
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
