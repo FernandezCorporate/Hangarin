97fd637afe228d00d2437bbff0ccf7e5c7fcd7b7
AllenFernandez <202380211@psu.palawan.edu.ph>
2026-03-01 23:30:40 +0800

    Added categorization filter

--- Hangarin_App/Task_and_Todo_Manager/views.py
+++ Hangarin_App/Task_and_Todo_Manager/views.py
@@ -18,18 +18,35 @@ class TaskView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
+        # 1. Start with the full list
         qs = super().get_queryset()
-        query = self.request.GET.get('q')
-
-        if query:
-            qs = qs.filter(
-                Q(title__icontains=query)
-            )
         
+        # 2. Get values from URL
+        query = self.request.GET.get('q')
+        p_val = self.request.GET.get('priority')
+        c_val = self.request.GET.get('category')
+        s_val = self.request.GET.get('status')
+
+        # 3. Apply Search (Only if query exists)
+        if query:
+            qs = qs.filter(title__icontains=query)
+
+        # 4. Apply Filters (Only if they are not "All" and not None)
+        if p_val and p_val != "All":
+            qs = qs.filter(priority__name__iexact=p_val)
+
+        if c_val and c_val != "All":
+            qs = qs.filter(category__name__iexact=c_val)
+
+        if s_val and s_val != "All":
+            qs = qs.filter(status__iexact=s_val)
+
         return qs
     
+
+    
     def get_ordering(self):
-        allowed = ["priority__name", "category__name", "status"]
+        allowed = ["priority", "category__name", "status", "deadline"]
         sort_by = self.request.GET.get("sort_by")
         if sort_by in allowed:
             return sort_by
